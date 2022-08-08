17733a9f8cf9d94b2302fc22ebdf7a7666aa3e29
anand <anand@Happy>
2022-08-08 09:50:30 +0530

    commited to local repository

---
+++ src/gitdemo11/gittest1.java
@@ -0,0 +1,21 @@
+package gitdemo11;
+
+public class gittest1 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		String s1="yes";
+		
+		if (s1=="yes")
+		{
+			System.out.println("watch movie");
+		}
+		else
+		{
+			System.out.println("go to bed");
+		}
+	
+	}
+}
